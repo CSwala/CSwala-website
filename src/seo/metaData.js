b1070c1a40c38511ab013e31e195dcb21076710d
Rituraj Jain <rrjain20122000@gmail.com>
2021-06-08 21:27:46 +0530

    Update metaData.js

--- src/seo/metaData.js
+++ src/seo/metaData.js
@@ -1,24 +1,24 @@
 const data = {
   home: {
-    title: "CSwala",
+    title: "CSwaala",
     description:
       "An organization to Educate people about the right style of getting educated!",
-    keywords: "CSwala, home, student, education",
+    keywords: "CSwaala, home, student, education",
   },
   notFound: {
-    title: "404 | CSwala",
+    title: "404 | CSwaala",
     description: "Uh ohh Looks like you encountered a 404 Not Found error.",
-    keywords: "CSwala, 404",
+    keywords: "CSwaala, 404",
   },
   explore: {
-    title: "Explore | CSwala",
+    title: "Explore | CSwaala",
     description: "Explore your field of interests in Computer Science",
-    keywords: "CSwala, explore, technology",
+    keywords: "CSwaala, explore, technology",
   },
   contact: {
-    title: "Contact us | CSwala",
+    title: "Contact us | CSwaala",
     description: "Contact and get in touch with us.",
-    keywords: "CSwala, contact, contact us",
+    keywords: "CSwaala, contact, contact us",
   },
 };
 
